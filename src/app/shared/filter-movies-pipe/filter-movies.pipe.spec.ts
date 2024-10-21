ea44df9ff5be0f6061d491fa2e9014ccbd802334
trinal0821 <trinal0821@gmail.com>
2024-10-20 20:54:21 -0600

    Auto-generated commit

--- src/app/shared/filter-movies-pipe/filter-movies.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterMoviesPipe } from './filter-movies.pipe';
-
-describe('FilterMoviesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterMoviesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
