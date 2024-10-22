d9965ffaeec0f240d12a855bbd88e02b45db72b1
trinal0821 <trinal0821@gmail.com>
2024-10-21 20:07:29 -0600

    Changed the order of the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { movieResolver } from './shared/resolver/movie.resolver';
 
 export const routes: Routes = [
-    { path: 'movies/:movieId', title: 'Movie Details', component: MovieDetailsComponent, resolve: {movie: movieResolver}},
     { path: 'movies', title: 'Movie List', component: MovieListComponent},  
+    { path: 'movies/:movieId', title: 'Movie Details', component: MovieDetailsComponent, resolve: {movie: movieResolver}},
     { path: '', redirectTo: '/movies', pathMatch: 'full'},
     { path: '**', redirectTo: '/movies'},
 ];
